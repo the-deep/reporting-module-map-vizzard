79cfd360d8dbcf34be5570d30a3eba4e30fb18f6
Matthew Lukin Smawfield <matthewsmawfield@gmail.com>
2023-06-02 01:53:08 +0300

    maplayers and mapvizzard

--- src/components/MapVizzard/MapVizzard.js
+++ src/components/MapVizzard/MapVizzard.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import PropTypes from 'prop-types';
-import './maplayers.css';
 import mapConfig from "./config.json";
 import Map from '../Map';
 import MapLayers from '../MapLayers';
