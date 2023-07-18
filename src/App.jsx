2a90852af33248a46657b64556051fc808122282
Matthew Lukin Smawfield <matthewsmawfield@gmail.com>
2023-07-18 10:35:27 +0300

    remove duplicate font imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
 import React from 'react';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
 import MapVizzard from './components/MapVizzard';
 import mapConfig from "./stories/mapConfig.json";
 import styles from './App.module.css';
