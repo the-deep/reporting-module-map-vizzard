00995ef96435e796173c8380ca7b0fee9ea4ffd4
Matthew Lukin Smawfield <matthewsmawfield@gmail.com>
2023-09-14 09:34:11 +0300

    embed url parameter (embed=true)

--- src/components/MapVizzard/MapVizzard.jsx
+++ src/components/MapVizzard/MapVizzard.jsx
@@ -11,28 +11,37 @@ function MapVizzard({ mapConfig }) {
   const [mapOptions, setMapOptions] = useState(mapConfig.mapOptions);
   const [activeLayer, setActiveLayer] = useState(null);
   const [mapObj, setMapObj] = useState(null);
+  const queryParameters = new URLSearchParams(window.location.search);
+  const embed = queryParameters.get('embed') || false;
+
+  console.log(mapOptions);
 
   return (
-    <div className="">
+    <div className={`${(embed ? styles.embed : '')}`}>
       <div className={`${styles.container}`}>
-        <div id="map_layers" className={`${styles.mapLayersContainer}`}>
-          <MapLayers
-            layers={layers}
-            setLayers={setLayers}
-            activeLayer={activeLayer}
-            setActiveLayer={setActiveLayer}
-          />
-        </div>
-        <div className={`${styles.mapOptionsContainer}`}>
-          <MapOptions
-            layers={layers}
-            setLayers={setLayers}
-            activeLayer={activeLayer}
-            mapOptions={mapOptions}
-            setMapOptions={setMapOptions}
-            mapObj={mapObj}
-          />
-        </div>
+        {!embed && (
+          <div id="map_layers" className={`${styles.mapLayersContainer}`}>
+            <MapLayers
+              layers={layers}
+              setLayers={setLayers}
+              activeLayer={activeLayer}
+              setActiveLayer={setActiveLayer}
+            />
+          </div>
+        )}
+        {!embed && (
+          <div className={`${styles.mapOptionsContainer}`}>
+            <MapOptions
+              layers={layers}
+              setLayers={setLayers}
+              activeLayer={activeLayer}
+              mapOptions={mapOptions}
+              setMapOptions={setMapOptions}
+              mapObj={mapObj}
+            />
+          </div>
+        )}
+
         <div id="map_panel" className={`${styles.mapPanel}`} style={{ fontFamily: mapOptions.fontStyle.fontFamily, color: rgba(mapOptions.fontStyle.color) }}>
           <Map
             layers={layers}
@@ -60,6 +69,7 @@ function MapVizzard({ mapConfig }) {
             showFooter={mapOptions.showFooter}
             sources={mapOptions.sources}
             showLogos={mapOptions.showLogos}
+            embed={embed}
           />
         </div>
       </div>
