35de9aa8387ae425c7d2d58debc5578370663bc4
Matthew Lukin Smawfield <matthewsmawfield@gmail.com>
2023-09-25 09:32:13 +0300

    proportional symbols, ukraine map

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import MapVizzard from './components/MapVizzard';
 import styles from './App.module.css';
 import sudan from './stories/sudan.json';
+import sudanBasemap from './stories/sudan-basemap.json';
 import ukraine from './stories/ukraine.json';
 
 const queryParameters = new URLSearchParams(window.location.search);
@@ -10,6 +11,8 @@ let mapConfig;
 
 if (config === 'ukraine.json') {
   mapConfig = ukraine;
+} else if (config === 'sudan-basemap.json') {
+  mapConfig = sudanBasemap;
 } else {
   mapConfig = sudan;
 }
